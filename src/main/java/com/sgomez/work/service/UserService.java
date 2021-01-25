64d4d9c5c6fdbaa395a5df5a3578aafc955a26a8
Santiago Gómez Vilar <sgvilar@esei.uvigo.es>
2021-01-25 23:10:20 +0100

    Adds logger

--- src/main/java/com/sgomez/work/service/UserService.java
+++ src/main/java/com/sgomez/work/service/UserService.java
@@ -5,6 +5,8 @@ import com.sgomez.work.api.model.UserResponse;
 import com.sgomez.work.exceptions.ResourceNotFoundException;
 import com.sgomez.work.repository.UserRepository;
 import com.sgomez.work.service.mapper.UserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     UserRepository userRepository;
+    Logger logger = LoggerFactory.getLogger(UserService.class);
+
 
     public UserResponse findById(String login) {
+        logger.info("First log");
         return userRepository.findById(login)
                 .map(UserMapper::entityToDto)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "login", login));
